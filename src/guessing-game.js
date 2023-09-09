873e9b2102ab5bd27ab1fec466bbf737d70157c6
gogicha007 <gogicha@gmail.com>
2023-09-09 12:58:57 +0400

    code done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,22 @@
 class GuessingGame {
-    constructor() {}
+    constructor() { }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return this.number = Math.ceil((this.min + this.max) / 2)
     }
 
     lower() {
+        this.max = this.number;
 
     }
 
     greater() {
-
+        this.min = this.number;
     }
 }
 
